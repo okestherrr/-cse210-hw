ba87001a930929c079d135b94b044dba9bae53cf
okestherrr <okesthers@byui.edu>
2025-04-29 10:00:34 -0600

    added the hw changes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello Sandbox World!");     
+        Console.Write("What is your first name?");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your first name?");
+        string lastName = Console.ReadLine();
+        Console.Write($"Your name is {lastName}, {firstName}.");
     }
 }
+

2e808b66d2e857e35c90f2e43b05977e4aaaf881
okestherrr <okesthers@byui.edu>
2025-05-02 19:53:43 -0600

    Homework C#prep3-loops done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,41 @@
 using System;
-
 class Program
 {
+    static int GetInt(string prompt)
+    {
+        Console.WriteLine(prompt);
+        string input = Console.ReadLine();
+
+        bool validInt = int.TryParse(input, out int number);
+
+        if (validInt) return number;
+
+        Console.WriteLine("Not a number, try again.");
+        return GetInt(prompt);
+
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new();
+        int ranNum = randomGenerator.Next(1, 101);
+        //Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("What is the magic number? ");
+        int guess = -1;
+        while (guess != ranNum)
+        {
+            guess = GetInt("What is your guess? ");
+            
+            if (guess > ranNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < ranNum)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+       
+        Console.WriteLine("You guessed it!");
     }
 }

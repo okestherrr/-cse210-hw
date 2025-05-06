9b0b9abcfbf7c3279619559709e148f521ca004f
okestherrr <okesthers@byui.edu>
2025-05-06 09:59:43 -0600

    added code to get area and radius of circle

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int num;
+        Console.Write("Enter a list of numbers, type 0 when finished. ");
+        
+        do
+        {
+            Console.WriteLine("Enter number: ");
+            string text = Console.ReadLine();
+            num = int.Parse(text);
+
+            if (num != 0)
+            {
+                numbers.Add(num);
+            }
+        while (num != 0);
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        // find avg and lg and sort
+        Console.Write($"The sum is: {sum}");
+        Console.Write($"The average is: {avg}");
+        Console.Write($"The largest number is: {lgn}");
+        Console.Write("The sorted list:");
+        Console.WriteLine(numbers, count);
+        
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,72 +1,45 @@
-class Program
+using System;
+using System.Diagnostics.Tracing;
+
+class Circle
 {
-    static double AddNumbers(double a, int b) //when using permaters you have to include the data inside as well
-    {
-        return a + b;  
-    }
-    static void Displaygreeting()
-    {
-        Console.WriteLine("Hello bob");
-    }
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Sandbox World!");     
-        Console.Write("What is your first name?");
-        string firstName = Console.ReadLine();
-        Console.Write("What is your last name?");
-        string lastName = Console.ReadLine();
-        Console.Write($"Your name is {lastName}, {firstName}.");
+    private double _radius;
 
-                // using System;
-        //using System.Data;
-        //using System.Numerics;
+    public void SetRadius(double radius)
+    {
+       if(radius < 0)
+       {
+        Console.WriteLine("Error");
+        return;
+       }
+        _radius = radius;
+    }
+    public double GetRadius()
+    {
+        return _radius;
+    } 
 
-        //class Program
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
     
-        //Console.WriteLine("Hello Learning03 World!");
+}
+class Program 
+{
+    static void Main(string[] args)
+    { // cicle is the class my circle is the object
+        Circle myCircle = new Circle();
+        Circle myCircle2 = new Circle();
+        myCircle.SetRadius(10);
+        //myCircle._radius = 10;
+        Console.WriteLine($"{myCircle.GetRadius()}");
 
-        // int x = 0;
-        // int y = x++; // this make y 1
-        // Console.WriteLine(y);
-        // y = ++x; // this make y 2
-        //Console.WriteLine(x);
+        myCircle2.SetRadius(20);
+        Console.WriteLine($"{myCircle2.GetRadius()}");
 
-        // || Or && AddingNewEventArgs ! not
+        Console.WriteLine($"{myCircle.GetArea()}");
 
-        // random randomGenerator = new Random();
-        // int randomNumber = new randomGenerator(1,100);
-
-        List <int> myNumbers = new List<int>();
-        myNumbers.Add(99);
-        myNumbers.Add(10);
-        myNumbers.Add(-123);
-        // myNumbers.
-
-        // Console.WriteLine("");
-
-        foreach (int i in myNumbers)
-        {
-           // Console.WriteLine(i);
-            // total += i;
-           //  Console.WriteLine(total);
-
-            double total = AddNumbers(123.4567, 987);
-            Console.WriteLine(total);
-            Console.WriteLine(AddNumbers(234,432));
-
-            Displaygreeting();
-
-            bool done = false; 
-            while(!done) // here is using while loop
-            {
-                done = true;
-            }
-
-            do{
-                Console.WriteLine("");
-            }
-            while(!done);
-        }
     }
 }
 

272fd64997fa1a434ffb1e6e1f699eebbbfac567
okestherrr <okesthers@byui.edu>
2025-07-08 10:36:16 -0600

    added test functions and started classes for final

---
+++ BudgetProject/income.cs
@@ -0,0 +1,5 @@
+
+class Income : Transaction
+{
+
+}
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,21 @@ using System;
 
 class Program
 {
+    static void TestByRef(int x, string n)
+    {
+        x++;
+        n += "plus betty";
+        Console.WriteLine($"In TestByRef: {x}, {n}");
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        int x = 10;
+        string name = "bob";
+
+        TestByRef(ref x, ref name);
+
+        Console.WriteLine($"in main: {x}, {name}");
+
     }
 }
